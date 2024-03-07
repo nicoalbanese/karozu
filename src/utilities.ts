985f0ded893c1abc955fdbfba57e099c95d18b64
Nico Albanese <gcalbanese96@gmail.com>
2024-03-07 16:14:51 +0000

    init implementation done

---
+++ src/utilities.ts
@@ -0,0 +1,25 @@
+export const baseUtilities = {
+  /**
+   * Transforms a string to CamelCase
+   *
+   * @param {string} s - String to transform
+   * @returns {string} Converted string
+   */
+  camelCase: (s: string) =>
+    s ? s.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase()) : "",
+  // ...add any other utility function
+  /**
+   * Transforms a string to UPPERCASE
+   *
+   * @param {string} input - String to transform
+   * @returns {string} Converted string
+   */
+  upperCase: (input: string) => input.toUpperCase(),
+  /**
+   * Transforms a string to lowercase
+   *
+   * @param {string} input - String to transform
+   * @returns {string} Converted string
+   */
+  lowerCase: (input: string) => input.toLowerCase(),
+};
---
+++ src/watcher.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import type { KarozuOptions } from "./index";
+import path from "path";
+import util from "util";
+
+// Convert fs functions to versions that return promises.
+// const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+let isCompiling = false;
+
+const updateConsole = (templateDir: string) => {
+  console.clear();
+  console.log("Karozu Watcher");
+  console.log("Listening for changes in:", templateDir);
+  console.log("---------------");
+};
+
+export function watch(options: KarozuOptions<any>) {
+  updateConsole(options.templateDir);
+  const templatesDir = path.join(process.cwd(), options.templateDir);
+  fs.watch(templatesDir, { recursive: true }, async (_, filename) => {
+    if (isCompiling) {
+      return;
+    }
+
+    if (!filename) {
+      return;
+    }
+
+    const ext = path.extname(filename);
+
+    if (ext === ".ts" && !filename.endsWith("compiled.ts")) {
+      isCompiling = true;
+      const filePath = path.join(templatesDir, filename);
+
+      // Clear the require cache to ensure new values are used on import
+      delete require.cache[require.resolve(filePath)];
+
+      // Import the TemplateFile instance
+      const templateMod = require(filePath);
+      const templateFile = templateMod.default;
+
+      if (
+        !templateFile ||
+        typeof templateFile.compileTestTemplate !== "function"
+      ) {
+        console.error(
+          `\nERROR:\nThe file doesn't export a TemplateFile instance.\n`,
+        );
+        isCompiling = false;
+        return;
+      }
+
+      // Compile
+      const compiledText = templateFile.compileTestTemplate();
+
+      // Write output text
+      const outputPath = filePath.replace(
+        "template.ts",
+        options.outputFileName,
+      );
+      await writeFile(outputPath, compiledText);
+
+      isCompiling = false;
+      console.log(
+        `[${new Date().toUTCString().split(" ").slice(4, 5)}] ` +
+          `File compiled: ${filePath.slice(outputPath.indexOf("src/"))}`,
+      );
+    }
+  });
+}
