30348b0a3c509c71843a10e72cadd9b8b0758239
Nico Albanese <nico.albanese@vercel.com>
2025-04-01 18:40:09 +0100

    export compiled template

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { Extension } from "./extension";
-import { Template, FileOperation, Replacement } from "./template";
+import { Template, FileOperation, Replacement, CompiledTemplate } from "./template";
 
 export { Extension, Template, FileOperation, Replacement };
